9e81211e8a3482f774495859b17f001e346b05d8
fedorrychkov <fedorrychkov@yandex.ru>
2019-03-14 02:02:16 +0300

    #4 add section layout for profile pages

---
+++ src/components/shared/Sections/PageSection.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="page-section">
+    <header class="page-section__head">
+      <div class="left">
+        <slot name="head-left"></slot>
+      </div>
+      <div class="right">
+        <slot name="head-right"></slot>
+      </div>
+    </header>
+    <div class="page-section__body">
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.page-section {
+  &__head {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
+</style>
