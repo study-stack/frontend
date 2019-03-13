5c876a9e952053596ac0b2703aec41365978b14a
fedorrychkov <fedorrychkov@yandex.ru>
2019-03-13 15:33:56 +0300

    add fontawesome icon plugin

---
+++ src/plugins/fontawesome.js
@@ -0,0 +1,9 @@
+import Vue from "vue";
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {} from "@fortawesome/free-solid-svg-icons";
+
+library.add();
+
+Vue.component(FontAwesomeIcon.name, FontAwesomeIcon);
--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,3 +1,4 @@
 import "./vuetify";
 import "./scrollto";
 import "./cookie";
+import "./fontawesome";
