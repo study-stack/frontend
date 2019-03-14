6b0c0bd281555607958e94caa7acd4e2cd2958e5
fedorrychkov <fedorrychkov@yandex.ru>
2019-03-15 00:16:48 +0300

    #3 add custom button component

---
+++ src/components/shared/ui/CustomButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <router-link
+    :to="link || '#'"
+    :class="{ 'button button__icon': true, button__invers: inverse }"
+  >
+    <span class="text">{{ title }}</span>
+    <div class="icon">
+      <div class="icon__container">
+        <div class="icon__left"></div>
+        <div class="icon__rigth"></div>
+      </div>
+    </div>
+  </router-link>
+</template>
+<script>
+export default {
+  props: {
+    link: String,
+    title: String,
+    inverse: Boolean
+  }
+};
+</script>
