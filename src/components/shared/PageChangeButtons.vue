be3d0eee19d39b74df421b06fa5a746e2bd10133
fedorrychkov <fedorrychkov@yandex.ru>
2019-03-18 00:38:49 +0300

    #3 add button for page change

---
+++ src/components/shared/PageChangeButtons.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="page-changer">
+    <button class="simple-button success" @click="changePage('prev')" :disabled="this.currentPage <= 1">Назад</button>
+    <button class="simple-button success" @click="changePage('next')" :disabled="this.currentPage >= this.pages">Следующий</button>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    pages: Number,
+    list: Array,
+    currentPage: Number
+  },
+  methods: {
+    changePage(type) {
+      if (type === 'prev') {
+        if (this.currentPage > 1) {
+          const page = this.currentPage - 1;
+          this.$emit('changePage', page);
+        }
+      }
+
+      if (type === 'next') {
+        if (this.currentPage < this.pages) {
+          const page = this.currentPage + 1;
+          this.$emit('changePage', page);
+        }
+      }
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.page-changer {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
