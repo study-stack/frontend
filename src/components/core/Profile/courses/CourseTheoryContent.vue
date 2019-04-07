d5efc0552d601cb7b34084e7fd943b6f5d543736
fedorrychkov <fedorrychkov@yandex.ru>
2019-04-07 18:32:17 +0300

    add theory content

---
+++ src/components/core/Profile/courses/CourseTheoryContent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="theory-content">
+    {{ content.content }}
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    content: Object
+  }
+};
+</script>
+<style lang="scss">
+.theory-content {
+  margin: 15px 0;
+  font-size: 18px;
+  line-height: 1.2;
+}
+</style>
