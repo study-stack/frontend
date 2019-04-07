893bd95cce91b000d3e127652f0944836d885d8e
fedorrychkov <fedorrychkov@yandex.ru>
2019-04-07 18:32:03 +0300

    add question type - without checkboxes

---
+++ src/components/core/Profile/courses/CourseAnswersSelect.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="question">
+    <h4 class="question-title">
+      {{ content.question.content }}
+    </h4>
+    <ul class="answer-list">
+      <li
+        class="answer-item"
+        v-for="item in content.question.answers"
+        :key="item.id"
+      >
+        {{ item.content }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    content: Object
+  }
+
+  // TODO: Add for questions step disabled state for NEXT button. If we haven't checked answers, we can't change step. And add send data for Answers type.
+};
+</script>
+<style lang="scss">
+.question {
+  margin: 15px 0;
+  font-size: 18px;
+  line-height: 1.2;
+
+  &-title {
+    font-weight: 400;
+    font-size: 20px;
+  }
+}
+
+.answer {
+  &-item {
+    margin: 8px 0;
+    font-weight: 600;
+  }
+}
+</style>
